3f72182d1408ee3a0ddc20b185511b0409995df5
suzukiplan <suzukiplan.ys@gmail.com>
2016-02-11 00:15:31 +0900

    formated

--- bin_src/vgs2play.c
+++ bin_src/vgs2play.c
@@ -11,7 +11,7 @@
 #include <time.h>
 #include "vgs2.h"
 
-int bload_direct(unsigned char n,const char* name);
+int bload_direct(unsigned char n, const char* name);
 void bfree_direct(unsigned char n);
 extern struct _PSG _psg;
 
@@ -21,13 +21,13 @@ extern struct _PSG _psg;
 int getsec(const char* text)
 {
     char* cp;
-    int ret=0;
-    cp=strchr(text,':');
-    if(NULL==cp) {
-        ret=atoi(text);
+    int ret = 0;
+    cp = strchr(text, ':');
+    if (NULL == cp) {
+        ret = atoi(text);
     } else {
-        ret=atoi(text)*60;
-        ret+=atoi(cp+1);
+        ret = atoi(text) * 60;
+        ret += atoi(cp + 1);
     }
     return ret;
 }
@@ -35,51 +35,51 @@ int getsec(const char* text)
 /**
  * main procedure
  */
-int main(int argc,char* argv[])
+int main(int argc, char* argv[])
 {
     char buf[1024];
-    int isPlaying=1;
-    int i,j;
-    int st=0;
+    int isPlaying = 1;
+    int i, j;
+    int st = 0;
     char* cp;
-    int show=0;
-    
+    int show = 0;
+
     /* check argument */
-    if(argc<2) {
+    if (argc < 2) {
         puts("usage: vgs2play bgm-file [mm:ss]");
         return 1;
     }
-    if(3<=argc) {
-        st=getsec(argv[2]);
+    if (3 <= argc) {
+        st = getsec(argv[2]);
     }
-    
+
     /* intialize sound system */
-    if(init_sound_cli()) {
-        fprintf(stderr,"Could not initialize the sound system.\n");
+    if (init_sound_cli()) {
+        fprintf(stderr, "Could not initialize the sound system.\n");
         return 2;
     }
 
 RELOAD:
     /* load BGM data and play */
-    if(bload_direct(0,argv[1])) {
-        fprintf(stderr,"Load error.\n");
+    if (bload_direct(0, argv[1])) {
+        fprintf(stderr, "Load error.\n");
         return 2;
     }
     vgs2_bplay(0);
-    if(st) vgs2_bjump(st);
+    if (st) vgs2_bjump(st);
 
-    if(!show) {
+    if (!show) {
         /* show song info */
         puts("Song info:");
-        printf("- number of notes = %d\n",_psg.idxnum);
-        if(_psg.timeI) {
-            printf("- intro time = %02u:%02u\n",_psg.timeI/22050/60, _psg.timeI/22050%60);
+        printf("- number of notes = %d\n", _psg.idxnum);
+        if (_psg.timeI) {
+            printf("- intro time = %02u:%02u\n", _psg.timeI / 22050 / 60, _psg.timeI / 22050 % 60);
         } else {
             printf("- acyclic song\n");
         }
-        printf("- play time = %02u:%02u\n",_psg.timeL/22050/60, _psg.timeL/22050%60);
+        printf("- play time = %02u:%02u\n", _psg.timeL / 22050 / 60, _psg.timeL / 22050 % 60);
         puts("");
-        
+
         /* show reference */
         puts("Command Reference:");
         puts("- p            : pause / resume");
@@ -92,72 +92,72 @@ RELOAD:
         puts("- r            : reload");
         puts("- q            : quit playing");
         puts("");
-        show=1;
+        show = 1;
     }
-    
+
     /* main loop */
-    memset(buf,0,sizeof(buf));
+    memset(buf, 0, sizeof(buf));
     printf("command: ");
-    while(NULL!=fgets(buf,sizeof(buf)-1,stdin)) {
-        for(i=0;buf[i];i++) {
-            if('A'<=buf[i] && buf[i]<='Z') {
-                buf[i]-='a'-'A';
+    while (NULL != fgets(buf, sizeof(buf) - 1, stdin)) {
+        for (i = 0; buf[i]; i++) {
+            if ('A' <= buf[i] && buf[i] <= 'Z') {
+                buf[i] -= 'a' - 'A';
             }
-            if(' '==buf[i] || '\t'==buf[i]) {
-                for(j=i;;j++) {
-                    buf[j]=buf[j+1];
-                    if(!buf[j]) break;
+            if (' ' == buf[i] || '\t' == buf[i]) {
+                for (j = i;; j++) {
+                    buf[j] = buf[j + 1];
+                    if (!buf[j]) break;
                 }
             }
         }
-        if(buf[0]=='p') {
-            if(isPlaying) {
+        if (buf[0] == 'p') {
+            if (isPlaying) {
                 vgs2_bstop();
-                isPlaying=0;
+                isPlaying = 0;
             } else {
                 vgs2_bresume();
-                isPlaying=1;
+                isPlaying = 1;
             }
-        } else if(buf[0]=='v') {
-            vgs2_bmvol(atoi(buf+1));
-        } else if(buf[0]=='c') {
-            if(3<=strlen(buf)) {
-                vgs2_bcvol((int)buf[1]-'0',atoi(buf+2));
+        } else if (buf[0] == 'v') {
+            vgs2_bmvol(atoi(buf + 1));
+        } else if (buf[0] == 'c') {
+            if (3 <= strlen(buf)) {
+                vgs2_bcvol((int)buf[1] - '0', atoi(buf + 2));
             }
-        } else if(buf[0]=='j') {
-            vgs2_bjump(getsec(buf+1));
-        } else if(buf[0]=='k') {
-            if(buf[1]=='+') {
+        } else if (buf[0] == 'j') {
+            vgs2_bjump(getsec(buf + 1));
+        } else if (buf[0] == 'k') {
+            if (buf[1] == '+') {
                 vgs2_bkey(atoi(&buf[2]));
-            } else if(buf[1]=='-') {
-                vgs2_bkey(0-atoi(&buf[2]));
+            } else if (buf[1] == '-') {
+                vgs2_bkey(0 - atoi(&buf[2]));
             }
-        } else if(buf[0]=='m') {
-            for(i=1;buf[i];i++) {
-                if('0'<=buf[i] && buf[i]<='5') {
-                    vgs2_bmute((int)(buf[i]-'0'));
+        } else if (buf[0] == 'm') {
+            for (i = 1; buf[i]; i++) {
+                if ('0' <= buf[i] && buf[i] <= '5') {
+                    vgs2_bmute((int)(buf[i] - '0'));
                 }
             }
-        } else if(buf[0]=='r') {
+        } else if (buf[0] == 'r') {
             bfree_direct(0);
             goto RELOAD;
-        } else if(buf[0]=='f') {
+        } else if (buf[0] == 'f') {
             vgs2_bfade2();
-        } else if(buf[0]=='q') {
+        } else if (buf[0] == 'q') {
             break;
         }
-        memset(buf,0,sizeof(buf));
+        memset(buf, 0, sizeof(buf));
         printf("command: ");
     }
-    
+
     /* terminate procedure */
     term_sound();
     bfree_direct(0);
-    
+
     return 0;
 }
 
-void putlog(const char* fn,int ln,const char* msg,...)
+void putlog(const char* fn, int ln, const char* msg, ...)
 {
 }
 
@@ -165,9 +165,9 @@ void make_pallet()
 {
 }
 
-FILE* vgs2_fopen(const char* n,const char* p)
+FILE* vgs2_fopen(const char* n, const char* p)
 {
-    return fopen(n,p);
+    return fopen(n, p);
 }
 
 void vgs2_showAds()
